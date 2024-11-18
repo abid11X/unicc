634f3fd96a26b1b1cbcba216aef92fd23bf26847
abid11X <newfb864@gmail.com>
2024-11-18 21:33:32 +0600

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ document
 
     try {
       // Send POST request to the server
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("https://unicc-backend-sooty.vercel.app/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
